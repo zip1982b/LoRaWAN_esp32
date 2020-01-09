ab76b2f36057ed843162fa61ba23d5ff5e6f6dc1
zip1982b <zip1982b@gmail.com>
2020-01-09 21:59:29 +0200

    homePC edit

--- proj/vent_sys/main/main.cpp
+++ proj/vent_sys/main/main.cpp
@@ -13,7 +13,7 @@
 #include "freertos/FreeRTOS.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
-
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 
 #include "TheThingsNetwork.h"
@@ -58,8 +58,35 @@ const char *appKey = "3d23df2458d8fda3aadb1c3f2a6e4fe8";
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<ZERO_SENSOR))
 #define ESP_INTR_FLAG_DEFAULT 0
 
+
+#define TIMER_DIVIDER         16  //  Hardware timer clock divider
+#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
+#define TIMER0_INTERVAL_SWITCH_ON_TRIAC   (0.00005) // for switch on TRIAC (sec) - 50uS
+#define TIMER1_INTERVAL_DELAY   (0.005)   // for firing angle to be 90' for 220V 50Hz AC signal, we need to have a delay of 5 ms
+#define WITHOUT_RELOAD   0        
+#define WITH_RELOAD      1       
+
+/*
+ * A sample structure to pass events
+ * from the timer interrupt handler to the main program.
+ */
+typedef struct {
+    int type;  // the type of timer's event
+    int timer_group;
+    int timer_idx;
+    uint64_t timer_counter_value;
+} timer_event_t;
+
+
+
+
+
+
+
+
 xQueueHandle xQueueDIM;
 xQueueHandle xQueueISR;
+xQueueHandle timer_queue;
 
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
@@ -71,8 +98,71 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
 	}
 }
 
+/*
+ * Timer group0 ISR handler
+ *
+ * Note:
+ * We don't call the timer API here because they are not declared with IRAM_ATTR.
+ * If we're okay with the timer irq not being serviced while SPI flash cache is disabled,
+ * we can allocate this interrupt without the ESP_INTR_FLAG_IRAM flag and use the normal API.
+ */
+void IRAM_ATTR timer_group0_isr(void *para)
+{
+    timer_spinlock_take(TIMER_GROUP_0);
+    int timer_idx = (int) para;
+
+    /* Retrieve the interrupt status and the counter value
+       from the timer that reported the interrupt */
+    uint32_t timer_intr = timer_group_get_intr_status_in_isr(TIMER_GROUP_0);
+    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(TIMER_GROUP_0, timer_idx);
+
+    /* Prepare basic event data
+       that will be then sent back to the main program task */
+    timer_event_t evt;
+    evt.timer_group = 0;
+    evt.timer_idx = timer_idx;
+    evt.timer_counter_value = timer_counter_value;
+
+    /* Clear the interrupt
+       and update the alarm time for the timer with without reload */
+    if (timer_intr & TIMER_INTR_T0) {
+        evt.type = TEST_WITHOUT_RELOAD;
+        timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, TIMER_0);
+        timer_counter_value += (uint64_t) (TIMER_INTERVAL0_SEC * TIMER_SCALE);
+        timer_group_set_alarm_value_in_isr(TIMER_GROUP_0, timer_idx, timer_counter_value);
+    } else if (timer_intr & TIMER_INTR_T1) {
+        evt.type = TEST_WITH_RELOAD;
+        timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, TIMER_1);
+    } else {
+        evt.type = -1; // not supported even type
+    }
+
+    /* After the alarm has been triggered
+      we need enable it again, so it is triggered the next time */
+    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, timer_idx);
+
+    /* Now just send the event data back to the main program task */
+    xQueueSendFromISR(timer_queue, &evt, NULL);
+    timer_spinlock_give(TIMER_GROUP_0);
+}
 
 
+static void venting(void* arg)
+{
+    
+    for(;;) {
+		
+		
+		
+		
+		
+		
+        //gpio_set_level(FAN_IN, 0);
+		vTaskDelay(5000 / portTICK_RATE_MS);
+        //gpio_set_level(FAN_IN, 1);
+		vTaskDelay(5000 / portTICK_RATE_MS);
+    }
+}
 
 
 
@@ -96,7 +186,41 @@ void sendMessages(void* pvParameter)
 }
 
 
+/*
+ * Initialize selected timer of the timer group 0
+ *
+ * timer_idx - the timer number to initialize
+ * auto_reload - should the timer auto reload on alarm?
+ * timer_interval_sec - the interval of alarm to set
+ */
+static void tg0_timer_init(int timer_idx,
+    bool auto_reload, double timer_interval_sec)
+{
+    /* Select and initialize basic parameters of the timer */
+    timer_config_t config;
+    config.divider = TIMER_DIVIDER;
+    config.counter_dir = TIMER_COUNT_UP;
+    config.counter_en = TIMER_PAUSE;
+    config.alarm_en = TIMER_ALARM_EN;
+    config.intr_type = TIMER_INTR_LEVEL;
+    config.auto_reload = auto_reload;
+#ifdef TIMER_GROUP_SUPPORTS_XTAL_CLOCK
+    config.clk_src = TIMER_SRC_CLK_APB;
+#endif
+    timer_init(TIMER_GROUP_0, timer_idx, &config);
 
+    /* Timer's counter will initially start from value below.
+       Also, if auto_reload is set, this value will be automatically reload on alarm */
+    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
+
+    /* Configure the alarm value and the interrupt on alarm. */
+    timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
+    timer_enable_intr(TIMER_GROUP_0, timer_idx);
+    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr,
+        (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+
+    timer_start(TIMER_GROUP_0, timer_idx);
+}
 
 
 
@@ -135,10 +259,18 @@ extern "C" void app_main(void)
     {
         printf("Joined.\n");
         xTaskCreate(sendMessages, "send_messages", 1024 * 4, (void* )0, 3, nullptr);
-		xTaskCreate(venting, "fan_work", 1024 * 4,  NULL, 11, NULL);
+		//xTaskCreate(venting, "fan_work", 1024 * 4,  NULL, 11, NULL);
     }
     else
     {
         printf("Join failed. Goodbye\n");
     }
+	
+	timer_queue = xQueueCreate(10, sizeof(timer_event_t));
+	tg0_timer_init(TIMER_0, WITH_RELOAD, TIMER0_INTERVAL_SWITCH_ON_TRIAC);
+    tg0_timer_init(TIMER_1, WITH_RELOAD, TIMER1_INTERVAL_DELAY);
+	xTaskCreate(venting, "test_fan_work", 1024 * 4,  NULL, 11, NULL);
+	
+	
+	
 }
